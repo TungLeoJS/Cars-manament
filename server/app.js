7620cc041e9cf4fdf9e3876290b3f4f4ba70aefa
Tung Nguyen <gtlduytung@gmail.com>
2022-10-21 19:44:27 +0700

    Init source

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('DB Connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+const app = express();
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', require('./v1/routes/index'));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${process.env.PORT || 5000}!`);
+});
+
+module.exports = app;
