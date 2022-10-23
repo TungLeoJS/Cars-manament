f1f836366bd63dca10ab8025ea91b798d0c76d76
Tung Nguyen <gtlduytung@gmail.com>
2022-10-23 20:06:43 +0700

    Add docker to dockerized application.

---
+++ README.md
@@ -0,0 +1,21 @@
+Pull the source code.
+
+Client:
+cd app
+
+npm install or yarn install
+
+npm run start / yarn start
+
+Server:
+cd server
+
+npm install/yarn install
+
+npm run start / yarn start
+
+Or you can use docker to start:
+
+docker-compose up
+Client: localhost:3000
+Backend: localhost:5000
--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { default: mongoose } = require('mongoose');
@@ -20,10 +18,8 @@ mongoose
 const app = express();
 
 app.use(cors())
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', require('./v1/routes/index'));
