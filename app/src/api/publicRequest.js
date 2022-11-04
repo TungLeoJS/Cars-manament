d057ee5d0853db1d9228d6c85ba046af70477eec
Tung Nguyen <gtlduytung@gmail.com>
2022-11-04 14:12:31 +0700

    Update base url.

--- app/src/api/publicRequest.js
+++ app/src/api/publicRequest.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const {
-  API_URL
-} = process.env;
-const BASE_URL = API_URL;
+// const {
+//   API_URL
+// } = process.env;
+const BASE_URL = 'http://165.22.54.199/api/v1';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
